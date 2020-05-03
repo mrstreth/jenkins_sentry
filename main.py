96390ab2fcde2055e61fa8f03c01a8b4d005235e
mrstreth <mr.tesanta@gmail.com>
2020-05-03 18:09:36 +0500

    fix bugs

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from typing import Union
-
 # необходимая настройка и импорт модулей
 import sentry_sdk
 sentry_sdk.init("https://8a1d87e02e734caab36501a3c56ba41c@o387100.ingest.sentry.io/5221989")
@@ -10,9 +8,6 @@ with configure_scope() as scope:
     scope.user = {'email':'mr.tesanta@gmail.com'}
     scope.set_tag("JENKINS","lab6")
     scope.set_tag("author","mrstreth")
-
-answer : Union[int, float] # анотация типа (не обязательна)	
-
 try:
     answer = 1 / 0
 except:
